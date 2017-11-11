c2b3610c44668f3953ad07eb17f28ef8cb34c9e6
Alexey Alekhin <laughedelic@gmail.com>
2017-11-11 21:42:49 +0100

    Ported server startup code to Scala.js :rocket:

--- src/main/scala/client.scala
+++ src/main/scala/client.scala
@@ -1,18 +1,24 @@
-import scala.scalajs.js
-import scala.scalajs.js.annotation._
-import js.Dynamic.global
-import io.scalajs.nodejs.child_process._
+import scala.scalajs.js, js.annotation._, js.Dynamic.global
+import io.scalajs.nodejs.child_process.ChildProcess
+import io.scalajs.nodejs.path.Path
+import io.scalajs.nodejs.os.OS
 
 @js.native
 @JSImport("atom-languageclient", "AutoLanguageClient")
 class AutoLanguageClient extends js.Object {
+  // Methods to export for Atom
   def activate(): Unit = js.native
   def deactivate(): Unit = js.native
 
+  // Methods obligatory to override
   def getGrammarScopes(): js.Array[String] = js.native
   def getLanguageName(): String = js.native
   def getServerName(): String = js.native
   def startServerProcess(projectPath: String): ChildProcess = js.native
+
+  val processStdErr: String = js.native
+  def captureServerErrors(childProcess: ChildProcess): Unit = js.native
+  def handleSpawnFailure(err: js.Any): Unit = js.native
 }
 
 object Exports {
@@ -30,11 +36,41 @@ class ScalaLanguageClient extends AutoLanguageClient {
 
   override def getGrammarScopes(): js.Array[String] = js.Array("source.scala")
   override def getLanguageName(): String = "Scala"
-  override def getServerName(): String = "Scala"
+  override def getServerName(): String = "Scalameta"
 
   override def startServerProcess(projectPath: String): ChildProcess = {
     global.console.log(s"startServerProcess(${projectPath})")
 
-    ChildProcess.spawn("node", js.Array("--version"))
+    // FIXME: use more civilized way of detecting JAVA_HOME
+    val javaHome = ChildProcess.asInstanceOf[js.Dynamic].execSync("/usr/libexec/java_home").toString.trim
+    val toolsJar = Path.join(javaHome, "lib", "tools.jar")
+
+    // TODO: try to use coursier directly
+    val coursierJar = Path.join(OS.homedir, "bin", "coursier")
+    val coursierArgs = js.Array(
+      "launch", "--quiet",
+      "--repository", "bintray:dhpcs/maven",
+      "--repository", "sonatype:releases",
+      "--extra-jars", toolsJar,
+      "org.scalameta:metaserver_2.12:0.1-SNAPSHOT",
+      "--main", "scala.meta.languageserver.Main"
+    )
+
+    val javaBin = Path.join(javaHome, "bin", "java")
+    val logLevel = "DEBUG"
+    val javaArgs = js.Array(
+      s"-Dvscode.workspace=${projectPath}",
+      s"-Dvscode.logLevel=${logLevel}",
+      "-jar", coursierJar
+    ).concat(coursierArgs)
+
+    global.console.log(javaArgs.mkString(javaBin, "\n", ""))
+
+    val serverProcess = ChildProcess.spawn(javaBin, javaArgs)
+    this.captureServerErrors(serverProcess)
+    serverProcess.on("exit", { err: js.Any =>
+      this.handleSpawnFailure(this.processStdErr)
+    })
+    serverProcess
   }
 }
